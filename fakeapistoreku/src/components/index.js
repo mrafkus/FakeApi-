04d51509a74fa7b5004ad3662bf67950416a3d8a
hvntss <97738905+mrafkus@users.noreply.github.com>
2023-01-18 23:17:00 +0700

    Update index.js

--- fakeapistoreku/src/components/index.js
+++ fakeapistoreku/src/components/index.js
@@ -2,5 +2,6 @@ import Footer from "./Footer";
 import Login from "./Login";
 import Navbar from "./Navbar";
 import Products from "./Products";
+import Users from "./Users";
 
-export {Footer, Login, Navbar, Products};
+export {Footer, Login, Navbar, Products, Users};
